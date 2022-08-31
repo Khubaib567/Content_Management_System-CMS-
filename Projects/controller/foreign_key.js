390c2711491c2cb14ec663585e36776d6f4905fe
Khubaib567 <49817481+Khubaib567@users.noreply.github.com>
2022-09-01 03:14:46 +0500

    Delete foreign_key.js

--- Projects/controller/foreign_key.js
+++
@@ -1,9 +0,0 @@
-
-module.exports.runQuery = function(user, users,project) {
-    return assign_key(user, users,project)
-}
-
-function assign_key(user, users,project) {
-    user = users.filter(obj => obj.user_name == project.project_created)
-    return user
-}
